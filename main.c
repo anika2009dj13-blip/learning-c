5ee2aca9c9eadbf998a9a5e01c74733cd07e184d
anika2009dj13-blip <anika2009dj13@gmail.com>
2026-08-05 20:46:31 +0530

    shopping cart

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+
 int main()
 {
-    printf("this thing worked!");
+   char item[50]="";
+   float price= 0.0f;
+   int quantity= 0;
+   char currency = '$';
+   float total =0.0f;
+   
+   printf("what item would you like to buy?:");
+   fgets(item,sizeof(item),stdin);
 
-    return 0;
+   printf("What is the price for each:");
+   scanf("%f",&price);
+   printf("how many would you like?:");
+   scanf("%d",&quantity);
+   total=price*quantity;
+   
+   printf("\nyou have bought %d %s\n",quantity,item);
+   printf("%c%.2f",currency,total);
+       return 0;
 }
