3bde48f3ba584b0364e915de6837e7131edbceee
anika2009dj13-blip <anika2009dj13@gmail.com>
2026-08-07 11:24:37 +0530

    Arithmetic calculator

---
+++ ArithmeticCalculator.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+int main()
+{
+char operator ='\0';
+double num1 = 0.0;
+double num2= 0.0;
+double result = 0.0;
+
+printf("Enter the first number:");
+scanf("%lf",&num1);
+
+printf("Enter the operators(+,-,*,/):");
+scanf(" %c",&operator);// since it had an input buffer i gave a space befor that %c
+
+printf("Enter the second number:");
+scanf("%lf",&num2);
+
+switch(operator){
+
+    case '+':
+    result = num1 + num2;
+    break;
+
+    case '-':
+    result = num1 - num2;
+    break;
+
+    case '*':
+    result = num1 * num2;
+    break;
+
+    case '/':
+    result = num1 / num2;
+    break;
+    default:
+    printf("Invalid operator\n");
+
+    
+}
+
+printf("Result:%.4lf",result);
+
+
+return 0;
+
+
+
+  
+
+
+
+
+
+
+}
